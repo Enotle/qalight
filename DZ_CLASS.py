942438a1073dce4ad0e6184f12adac3542f74f31
Aleksandra Din <enotle.eltone@gmail.com>
2023-05-25 14:35:07 +0300

    моя домашка

---
+++ DZ_CLASS.py
@@ -0,0 +1,10 @@
+class human():
+
+    favorite_drink = 'beer'
+    def drink(self):
+        print(favorite_drink)
+    def __init__(self, age):
+        self.age = int
+
+
+Sasha = human.drink(5)
---
+++ class.py
@@ -0,0 +1,6 @@
+def func(arg: int|str):
+    print(str(arg) + ' -------- ' + str(arg)) ###число преобраз. в строку
+
+func(5)
+func(2)
+func(345)
